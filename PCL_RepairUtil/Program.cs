bd12e3ede1be3b2dbfb934ab8de042b2df339f3e
Paul-N <evilgenius@211.ru>
2014-05-15 03:42:13 +0700

    Init commit

---
+++ PCL_RepairUtil/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace PCL_RepairUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine("You have to provide path to PCL installation");
+                return;
+            }
+
+            var pathToPcl = args[0] + "\\v4.0\\Profile\\";
+
+            if (!Directory.Exists(pathToPcl))
+            {
+                Console.WriteLine("Wrong path to PCL installation");
+                return;
+            }
+
+            foreach (var profile in Directory.GetDirectories(pathToPcl))
+            {
+                var suppFrmw = profile + "\\SupportedFrameworks\\";
+
+                if (Directory.Exists(suppFrmw))
+                {
+                    foreach (var xmlFile in Directory.GetFiles(suppFrmw).Where(f => f.EndsWith(".xml")))
+                    {
+                        var xml = XDocument.Load(xmlFile);
+
+                        var attr = xml.Elements().ElementAt(0).Attributes().SingleOrDefault(a => a.Name == "MaximumVisualStudioVersion");
+
+                        if (attr != null)
+                        {
+                            attr.Value = "12.0";
+                            xml.Save(xmlFile);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
